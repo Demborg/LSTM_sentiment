d1bbae80eaa8f3b3cac1003bcaf9e90450e4d4b3
Marten <marten3@kth.se>
2017-12-11 13:44:36 +0100

    small modifications on dataset

--- datasets.py
+++ datasets.py
@@ -1,5 +1,6 @@
 import torch
 import json
+import sys
 
 from torch.utils.data import Dataset
 
@@ -7,15 +8,17 @@ class YelpReviews(Dataset):
     def __init__(self, path):
         self.file = path
         self.len = 0
+        print("Dataset: going through all reviews...")
         with open(path) as f:
             for line in f:
                 self.len += 1
+        print("Dataset: Is ready!")
 
     def __len__(self):
         return self.len
 
 if __name__ == "__main__":
-    dataset = YelpReviews("small_data.json")
+    dataset = YelpReviews(sys.argv[1])
     print(len(dataset))
                 
 
