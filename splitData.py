cd713652d42dac226ecfc4c8e367aac1235fd63b
Axel Demborg <axel.demborg@gmail.com>
2017-12-12 15:17:52 +0100

    fixes mårtens nit pick

--- splitData.py
+++ splitData.py
@@ -4,7 +4,9 @@ import sys
 random.seed(1337)
 
 def split(filename, test=0.1, validate=0.1):
-    """Takes a file and the percentages that should go to test and validation and creates three new files filename_test, filename_train and filename_validation with the given proportion of lines from the original file"""
+    """Takes a file and the percentages that should go to test and validation and creates three new
+    files filename_test, filename_train and filename_validation with the given proportion of lines
+    from the original file"""
 
     if test < 0 or validate < 0 or test + validate > 1:
         raise ValueError('validate and test are the proportions that go to test and validation sets, make sure they are set to sensible values')
