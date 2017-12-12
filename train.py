ecf4593e4b2772b5b2f3eddd087d2880bea21b7d
Marten <marten3@kth.se>
2017-12-12 14:55:13 +0100

    Applied checkpointing in train.

--- train.py
+++ train.py
@@ -1,18 +1,22 @@
+import sys
+
 import torch
 from torch.utils.data import DataLoader
-import sys
-from datasets import YelpReviews
-import settings
-import models
 from torch.autograd import Variable
 from visdom import Visdom
 import numpy as np
 
+import datasets
+import settings
+import models
+import utils
+
+
 # Instansiate dataset
-dataset = YelpReviews(settings.DATAFILE)
+dataset = datasets.YelpReviews(settings.DATAFILE)
+data_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=4)
 
 # Define model and optimizer
-# model = models.BaselineModel(settings.HIDDEN_SIZE)
 model = models.SimpleLSTM(settings.HIDDEN_SIZE)
 optimizer = torch.optim.Adam(model.parameters(), lr=settings.LEARNING_RATE)
 
@@ -21,26 +25,40 @@ viz = Visdom()
 loss_plot = viz.line(X=np.array([0]), Y=np.array([0]))
 dist_hist = viz.histogram(X=np.random.rand(100), opts=dict(numbins=20))
 
-data_loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=4)
-
 counter = 0
 for epoch in range(settings.EPOCHS):
+    # Stars for histogram
     stars = np.zeros(len(dataset))
+
+    # Main epoch loop
+    length = len(dataset)
+    print("Starting epoch {} with length {}".format(epoch, length))
     for i, (feature, target) in enumerate(data_loader):
-        feature = Variable(feature.permute(1,0,2))
+        # Inference
+        feature = Variable(feature.permute(1, 0, 2))
         target = Variable(target)
-        out=model(feature)
-        stars[i] = out[-1,0,0]
-        loss = torch.mean((out[-1,0] - target)**2)
-        viz.line(win=loss_plot, X=np.array([counter]), Y=loss.data.numpy(), update='append')
+        out = model(feature)
+
+        # Loss computation and weight update step
+        loss = torch.mean((out[-1, 0] - target)**2)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        #print("Label: {}".format(target))
-        # print("Prediction: {}".format(out[-1]))
-        # print("Loss: {}".format(loss))
 
-        counter+=1
+        # Visualization update
+        stars[i] = out[-1, 0, 0]
+        viz.line(win=loss_plot, X=np.array([counter]), Y=loss.data.numpy(), update='append')
+        counter += 1
+
+        # Progress update
+        if i % 10 == 0:
+            sys.stdout.write("\rIter {}/{}, loss: {}".format(i, length, float(loss)))
+    print("Epoch finished with last loss: {}".format(loss))
+
+    # Visualize distribution and save model checkpoint
     viz.histogram(win=dist_hist, X=stars, opts=dict(numbins=20))
-    print(stars)
+    name = "test_save_epoch{}.params".format(epoch)
+    utils.save_model_params(model, name)
+    print("Saved model params as: {}".format(name))
+
 
